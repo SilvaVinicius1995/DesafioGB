2e4fa3fbe2f2df9d48371ae4aa10c14d8af22504
SilvaVinicius1995 <vinirodrigues59@gmail.com>
2021-11-30 16:37:34 -0300

    main
    ajuste query

--- dags/python/Tweepy.py
+++ dags/python/Tweepy.py
@@ -22,7 +22,11 @@ engineMysql =("mysql+pymysql://"+username+":"+password+"@localhost:3306/"+schema
 engine = sqlalchemy.create_engine(engineMysql)
 
 # selecionando nome da linha com mais vendas no mês 12 de 2019
-sqlQuery = """SELECT MAX(LINHA) as LINHA FROM `CONSOLIDADO_VENDAS_LINHA-ANO-MES` WHERE ANO = 2019 AND MES = 12"""
+sqlQuery = """SELECT LINHA FROM `CONSOLIDADO_VENDAS_LINHA-ANO-MES` 
+WHERE QTD_VENDA = 
+(SELECT MAX(QTD_VENDA) FROM `CONSOLIDADO_VENDAS_LINHA-ANO-MES` 
+WHERE ANO = 2019 AND MES = 12)"""
+
 df_query = pd.read_sql_query(sqlQuery,engine)
 linha = df_query['LINHA'].values[0]
 
@@ -39,16 +43,17 @@ cursor_tweet = tw.Cursor(api.search_tweets,q=query,lang='pt',tweet_mode="extende
 
 # criando dicionario
 tweets_dict = {}
-tweets_dict = tweets_dict.fromkeys(['created_at',  'full_text', 'user'])
+tweets_dict = tweets_dict.fromkeys(['id','created_at',  'full_text', 'user'])
 list_dict = []
 
 # Executando cursor
 for tweet in cursor_tweet:
     for key in tweets_dict.keys():
+        id = tweet._json['id']
         created_at = tweet._json['created_at']
         user = tweet._json['user']['screen_name']
         full_text = tweet._json['full_text']
-        data = {"created_at": created_at, "user": user, "full_text": full_text}
+        data = {"id": id,"created_at": created_at, "user": user, "full_text": full_text}
         list_dict.append(data)
 
 # criando dataframe
@@ -60,6 +65,6 @@ df = df.drop_duplicates()
 df.to_sql(
     name = 'TWEETS',
     con = engine,
-    index = False,
+    index = True,
     if_exists ='append'
 )
